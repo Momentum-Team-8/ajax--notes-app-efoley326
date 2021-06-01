09a7b3ae2cb77adc4ddcfc5ee5675a78b35b3e89
Emily Foley <efoley326@gmail.com>
2021-06-01 09:17:20 -0400

    work I did on the plane

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
 const url = 'http://localhost:3000/notes'
-const form = 'http://localhost:3000/notes/'
-const Notepad = document.querySelector('#notepad')
-
+const noteInput = document.querySelector('#notepad')
+const output = document.querySelector('#output')
 {
-fetch (url)
-.then ((Response) = response.json())
-.then ((data) => console.log (data))
-}
+    function getNotes () {
+    fetch (url), 
+        .then ((response) => data.json()) 
+        .then; document.createElement('p').innerHTML = data.json + output.appendChild (output =>
+    }
+
+        for (let d of data)
+
+        {
+        let title = document.createElement('p')
+        title.innerHTML = "Title" + output ;
+        Title.appendChild(title)
+        }
+        {
+        let body = document.createElement('p');
+        body.innerHtml = "Body:" + x.body
+        output.appendChild(body)
+        }
+
+        document.getElementById("submit").addEventListener("click", event => {
+            event.preventDefault()
+            getNotes()
+        })
+    }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    {
+        method: 'POST', 
+        headers: {"Content-Type": "application/json"}, 
+        body: JSON.stringify({"title": "Hi", "body": "COOL"})
+      })
+      .then(r => r.json())
+      .then(console.log (r.json)) ;
+    }
